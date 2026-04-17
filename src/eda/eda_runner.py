d7575e250d729bbfcadc62a296a97cf7be8b835e
Manu_i_003 <manikantathota2003@gmail.com>
2026-04-17 20:10:41 +0530

    fix: data preprocessing stuff removed from eda_runner module

--- src/eda/eda_runner.py
+++ src/eda/eda_runner.py
@@ -1,15 +1,7 @@
-"""
-run_eda.py
+from pathlib import Path
+import pandas as pd
 
-Runs full EDA pipeline:
-- Generates plots
-- Saves them automatically
-"""
-
-from src.data_ingestion.load_data import load_all_data
-from src.data_preprocessing.transformation import convert_date
-from src.data_preprocessing.merging import merge_datasets
-from src.data_preprocessing.cleaning import handle_missing_values, fix_holiday_columns
+from src.config.config import INTERIM_DATA_DIR
 
 from src.eda.univariate import sales_distribution
 from src.eda.bivariate import sales_by_store, holiday_impact
@@ -20,34 +12,36 @@ from src.eda.visualization import save_plot
 def run_eda_pipeline():
     print("\n==========( EDA Pipeline Started )==========\n")
 
-    # Load and preprocess data
-    sales, stores, features = load_all_data()
+    # ----------------------------------------------------
+    # Load preprocessed (interim) data
+    # ----------------------------------------------------
+    file_path = INTERIM_DATA_DIR / "merged_data.csv"
 
-    sales = convert_date(sales)
-    features = convert_date(features)
+    if not file_path.exists():
+        raise FileNotFoundError("(✕) -> Interim data not found. Run preprocessing first.")
 
-    df = merge_datasets(sales, stores, features)
-    df = handle_missing_values(df)
-    df = fix_holiday_columns(df)
+    df = pd.read_csv(file_path, parse_dates=["Date"])
 
-    # -----------------------------
+    print(f"(✓) -> Loaded interim data | Shape: {df.shape}")
+
+    # ----------------------------------------------------
     # Univariate
-    # -----------------------------
+    # ----------------------------------------------------
     plot = sales_distribution(df)
     save_plot("sales_distribution")
 
-    # -----------------------------
+    # ----------------------------------------------------
     # Bivariate
-    # -----------------------------
+    # ----------------------------------------------------
     plot = sales_by_store(df)
     save_plot("sales_by_store")
 
     plot = holiday_impact(df)
     save_plot("holiday_impact")
 
-    # -----------------------------
+    # ----------------------------------------------------
     # Multivariate
-    # -----------------------------
+    # ----------------------------------------------------
     plot = monthly_trend(df)
     save_plot("monthly_trend")
 
