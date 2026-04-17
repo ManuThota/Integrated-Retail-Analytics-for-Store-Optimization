8fcf7b7715ccd43048629c15e6ba25f110028b82
Manu_i_003 <manikantathota2003@gmail.com>
2026-04-17 22:35:53 +0530

    fix: minor fixes

--- src/data_preprocessing/saving.py
+++ src/data_preprocessing/saving.py
@@ -65,3 +65,20 @@ def save_processed_data(df: pd.DataFrame, filename: str = "processed_data.csv") 
     df.to_csv(file_path, index=False)
 
     print(f"(✓) -> Processed data saved at: {file_path}")
+
+# ============================================================
+# Save Final ML Dataset
+# ============================================================
+
+def save_ml_dataset(df: pd.DataFrame, filename: str = "ml_dataset.csv") -> None:
+    """
+    Saves final machine learning dataset.
+    """
+
+    PROCESSED_DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+    file_path = PROCESSED_DATA_DIR / filename
+
+    df.to_csv(file_path, index=False)
+
+    print(f"(✓) -> ML dataset saved at: {file_path}")
