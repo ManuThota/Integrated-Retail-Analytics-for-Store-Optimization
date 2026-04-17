1a235554bf267dac2b938553d7c8998c207d8684
Manu_i_003 <manikantathota2003@gmail.com>
2026-04-17 19:47:39 +0530

    feat: added merge validation to merge data function

--- src/data_preprocessing/merging.py
+++ src/data_preprocessing/merging.py
@@ -14,34 +14,37 @@ import pandas as pd
 #============================================================
 # Merging the Datasets
 #============================================================
-def merge_datasets(sales_df: pd.DataFrame,
-                   stores_df: pd.DataFrame,
-                   features_df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Merges sales, stores, and features datasets.
-
-    Returns:
-        pd.DataFrame: Merged dataframe
-    """
+def merge_datasets(sales_df, stores_df, features_df):
 
     print("Merging datasets...")
 
-    # Merge sales + features on Store & Date
+    # Debug: check duplicates
+    print("\nChecking key uniqueness...")
+
+    sales_duplicates = sales_df.duplicated(subset=["Store", "Date"]).sum()
+    features_duplicates = features_df.duplicated(subset=["Store", "Date"]).sum()
+
+    print(f"Sales duplicates (Store, Date): {sales_duplicates}")
+    print(f"Features duplicates (Store, Date): {features_duplicates}")
+
+    # Merge sales + features
     df = pd.merge(
         sales_df,
         features_df,
         on=["Store", "Date"],
-        how="left"
+        how="left",
+        validate="many_to_one" 
     )
 
-    # Merge with store data
+    # Merge stores
     df = pd.merge(
         df,
         stores_df,
         on="Store",
-        how="left"
+        how="left",
+        validate="many_to_one"
     )
 
-    print(f"(✓) -> Merging completed | Shape: {df.shape}")
+    print(f"(✓) ->Merging completed | Shape: {df.shape}")
 
     return df
